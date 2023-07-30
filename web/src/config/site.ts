cf9030f6d2c1fc7cb1ccea47025ee482d5b81715
Quatton <atomicatives@gmail.com>
2023-07-31 03:38:39 +0900

    web: last fix

--- web/src/config/site.ts
+++ web/src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: "Callem",
+  title: "Call'em",
   description:
     "Clone your voice with ElevenLabs and direct your calls to ChatGPT",
   phone: Bun.env.PHONE_NUMBER,
